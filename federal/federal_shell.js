033e9a9ded707bd44440e6dcf4670ca54eda7d77
loterpraca <loterpraca@gmail.com>
2026-04-12 19:00:15 -0300

    Update federal_shell.js

--- federal/federal_shell.js
+++ federal/federal_shell.js
@@ -1,1 +1,10 @@
 
+const federalState = {
+  usuario: null,
+  perfil: null,
+  loterias: [],
+  loteriaPrincipalId: null,
+  lojaSelecionada: null,
+  concursoSelecionado: null,
+  dataReferencia: new Date().toISOString().slice(0, 10)
+};
